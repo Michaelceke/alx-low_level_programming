2c091d6411bfe1a9ac10cece9fc97d9314989a65
Michaelceke <ekemicheal@gmail.com>
2022-06-05 16:45:08 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print lowercase and uppercase a-zA-Z using putchar
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char lowercase = 'a;
+
+	while (lowercase <= 'z')
+	{
+		putchar(lowercase);
+		lowercase++;
+	}
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print lowercase and uppercase a-zA-Z using putchar
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char lowercase = 'a';
+	char uppercase ='A';
+
+	while (lowercase <= 'z')
+	{
+		putchar(lowercase);
+		lowercase++;
+	}
+
+	while (uppercase <= 'Z')
+	{
+		putchar(uppercase);
+		uppercase++;
+	}
+
+	return (0);
+}
