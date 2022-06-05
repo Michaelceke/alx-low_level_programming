1ad8dda822e491a1d182ee4fb47e736b89b83781
Michaelceke <ekemicheal@gmail.com>
2022-06-04 21:20:22 -0700

    6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ int main(void)
 	float floater;
 	long long longer_throat;
 
-	printf("size of a char is %u byte(s)\n", sizeof(charchar));
-	printf("Size of an int: %u byte(s)\n", sizeof(intint));
-	printf("Size of a long int: %u byte(s)\n", sizeof(longer));
-	printf("Size of a float: %u byte(s)\n", sizeof(floater));
-	printf("Size of a long long: %u byte(s)\n", sizeof(longer_throat));
+	printf("size of a char is % byte(s)\n", sizeof(charchar));
+	printf("Size of an int: % byte(s)\n", sizeof(intint));
+	printf("Size of a long int: % byte(s)\n", sizeof(longer));
+	printf("Size of a float: % byte(s)\n", sizeof(floater));
+	printf("Size of a long long: % byte(s)\n", sizeof(longer_throat));
 
 	return (0);
 }
