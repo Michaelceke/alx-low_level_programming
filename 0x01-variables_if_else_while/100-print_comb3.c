e4f8c8ced99d586c779c04ad1b62bd5846311d29
Michaelceke <ekemicheal@gmail.com>
2022-06-06 14:36:55 -0700

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - print possible combination of single-digit numbet printing
+ * from 0-9 separated with commas, using putchar
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int tens = '0';
+	int unit;
+
+	while (tens <= '9')
+	{
+
+		unit = tens + 1;
+
+		while (unit <= '9')
+
+		{
+			putchar(tens);
+			putchar(unit);
+
+			unit++;
+
+			if (tens != '8' ||  unit != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+
+		}
+
+		tens++;
+	}
+	putchar('\n');
+
+	return (0);
+}
