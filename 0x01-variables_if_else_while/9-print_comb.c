1008890c2554ab3f26e8533bb5ce869c3683d609
Michaelceke <ekemicheal@gmail.com>
2022-06-06 12:23:50 -0700

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - print possible combination of single-digit numbet printing
+ * from 0-9 separated with commas, using putchar
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int counter = '0';
+
+	while (counter <= '9')
+	{
+		putchar(counter);
+
+		if (counter != '9')
+		{
+			putchar(',');
+			
+			putchar(' ');
+		}
+
+		counter++;
+	}
+	putchar('\n');
+
+	return (0);
+}
