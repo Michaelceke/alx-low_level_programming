5e468e6661c13674d61d87045bf2e7fd10ac7f0f
Michaelceke <ekemicheal@gmail.com>
2022-06-08 12:24:13 -0700

    print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Print alphabet in lowercase 10 times using
+ * only _putchar
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		a = 'a';
+
+		while (a < 'z')
+		{
+			_putchar(a);
+			a++;
+		}
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/temp.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_alphabet - Print alphabet in lowercase using only _putchar
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	_putchar('\n');
+}
