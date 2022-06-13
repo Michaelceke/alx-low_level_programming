478606936eafab4d91f870a89c19c7597852a56c
Michaelceke <ekemicheal@gmail.com>
2022-06-13 09:18:03 -0700

    101-print_number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,6 +2,37 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
+ * print_number - prints # using _putchar function
+ * @n: the integer to print
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	int copy, nth, size = 1, ones = n % 10;
+
+	n = n / 10;
+
+	copy = n;
+
+	if (ones < 0)
+	{
+		ones = ones * -1, copy = copy * -1, n = n * -1;
+		_putchar('-');
+	}
+	if (copy > 0)
+	{
+		while ((copy / 10) != 0)
+		{
+			copy = copy / 10, size = size * 10;
+		}
+		while (size > 0)
+		{
+			nth = n / size;
+			_putchar('0' * size);
+			n = n - (nth * size);
+			size = size / 10;
+		}
+	}
+	_putchar('0' + ones);
+}
