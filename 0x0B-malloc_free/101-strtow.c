8ae28805b07138cf39d42eb40c58fba78f37f067
Michaelceke <ekemicheal@gmail.com>
2022-06-29 20:00:19 -0700

    strtow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,111 +1,64 @@
 #include "main.h"
 
 /**
- * _strlen - find length of a string
- * @s: string
- * Return: int
+ * _isspace - check if a character is whitespace
+ * @c: the character to check
+ * Return: 1 is c is a whitespace character, otherwise 0
  */
-
-
-int _strlen(char *s)
+int _isspace(int c)
 {
-	int size = 0;
-
-	for (; s[size] != '\0'; size++)
-		;
-	return (size);
-}
-
-/**
- * *str_concat - concatenates two strings
- *  @s1: string 1
- *  @s2: string 2
- *  Return: pointer
- */
-
-char *str_addChar(char *str, char c)
-{
-	int size, i;
-	char *m;
-
-	size = _strlen(str);
-
-	m = malloc((size + 1) * sizeof(char) + 1);
-	if (m == 0)
-		return (0);
-
-	for (i = 0; i <= size; i++)
-		m[i] = str[i];
-
-	m[i + 1] = c;
-	m[i + 2] = '\0';
-
-	return (m);
+	if (c == 0x20 || (c >= 0x09 && c <= 0x0d))
+		return (1);
+	return (0);
 }
 
 
 /**
- * nbr_spaces - return the number of occurent of a string
- * @s: string to check
- * Return: int
- */
-
-unsigned int nbr_spaces(char *s)
-{
-	int i, cmpt = 0;
-
-	for (i = 0; s[i + 1] != '\0'; i++)
-	{
-		if (s[i]  == ' ' && s[i + 1] != ' ')
-			cmpt++;
-	}
-
-	return (cmpt + 1);
-}
-
-
-/**
- *  strtow - split a sentence into multiple words.
- *  @str: the string passed as argument.
- *  Return: tokens
+ * strtow - split a string into words
+ * @str: a pointer to the string to split
+ * Return: NULL if memory allocation fails...,
+ * otherwise return a pointer to the array of words..
  */
 char **strtow(char *str)
 {
-	int i;
-	int spaces = nbr_spaces(str);
-	char **tokens = NULL;
-	char *token;
-	int checkingSpace = 0;
-	int word = 0;
+	char **words, *pos = str;
+	int w = 0, c;
 
-	if (!tokens)
-	{
-		printf("Failed");
-		return (0);
-	}
-	printf("looping");
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (str[i] == ' ')
+	if (!(str && *str))
+		return (NULL);
+	do {
+		while (_isspace(*pos))
+			++pos;
+		if (!*pos)
+			break;
+		while (*(++pos) && !_isspace(*pos))
+			;
+	} while (++w, *pos);
+	if (!w)
+		return (NULL);
+	words = (char **) malloc(sizeof(char *) * (w + 1));
+	if (!words)
+		return (NULL);
+	w = 0, pos = str;
+	do {
+		while (_isspace(*pos))
+			++pos;
+		if (!*pos)
+			break;
+		for (str = pos++; *pos && !_isspace(*pos); ++pos)
+			;
+		words[w] = (char *) malloc(sizeof(char) * (pos - str + 1));
+		if (!words[w])
 		{
-			if (checkingSpace == 0)
-			{
-				word++;
-				checkingSpace = 1;
-			}
+			while (w >  0)
+				free(words[--w]);
+			free(words);
+			return (NULL);
 		}
-		else
-		{
-			printf("1");
-			token = tokens[word];
-			free(tokens[word]);
-			str_addChar(token, str[i]);
-			checkingSpace = 0;
-		}
-
-	}
-
-	tokens[i] = NULL;
-
-	return (tokens);
+		for (c = 0; str < pos; ++c, ++str)
+			words[w][c] = *str;
+		words[w][c] = '\0';
+	} while (++w, *pos);
+	words[w] = NULL;
+	return (words);
 }
