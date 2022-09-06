677629e336069387e535ef853629ddd6ed059113
Michaelceke <ekemicheal@gmail.com>
2022-09-05 20:26:15 -0700

    created libdynamic.so

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_abs - Return the absolute value of an integer parameter
+ * @n: int type number
+ * Return: Absolute value of n
+ */
+
+int _abs(int n)
+{
+	if (n > 0 || n == 0)
+	{
+		return (n);
+	}
+	else
+		return (-1 * n);
+}
